4be97dbbfa9ee8d5b2150731153734dc733d7709
mnolting <matthew.nolting@gmail.com>
2014-10-21 23:42:58 -0400

    done

--- assets/js/_script.js
+++ assets/js/_script.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-	$("h1 #line1").fitText(1.1);
-	$("h1 #line2").fitText(.8);
-	$("h1 #line3").fitText();
-	$("h1 #line4").fitText();
+jQuery(document).ready(function($) {
+	$("#line1").fitText(.82);
+	$("#line2").fitText(.8);
+	$("#line3").fitText(1.25);
+	$("#line4").fitText(.75);
+	$("#line5").fitText(1.1);
+	$("#line6").fitText(.8);
+	$("#line7").fitText();
+	$("#line8").fitText(1.25);
 });
