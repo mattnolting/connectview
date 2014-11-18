dcf0d0d2c6ab00bed3471a802918a3733151a69a
mnolting <matthew.nolting@gmail.com>
2014-11-18 15:17:54 -0500

    update education page

--- assets/js/_script.js
+++ assets/js/_script.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-	$("#line1").fitText(1.2);
-	$("#line2").fitText(.71);
+	$("#line1").fitText(1.3);
+	$("#line2").fitText(.6);
 	$("#line3").fitText(1.1);
 	$("#line4").fitText(.83);
 	$("#line5").fitText(1.1);
-	$("#line6").fitText(.8);
-	$("#line7").fitText();
-	$("#line8").fitText(1.25);
+	$("#line6").fitText(1);
+	$("#line7").fitText(0.6);
+	$("#line8").fitText(1.2);
+	$("#line9").fitText(.9);
 });
