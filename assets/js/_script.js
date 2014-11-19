ca929a46198ee4be08dc7f594c4085b538237d6d
mnolting <matthew.nolting@gmail.com>
2014-11-19 01:16:46 -0500

    update education and business

--- assets/js/_script.js
+++ assets/js/_script.js
@@ -4,8 +4,14 @@ jQuery(document).ready(function($) {
 	$("#line3").fitText(1.1);
 	$("#line4").fitText(.83);
 	$("#line5").fitText(1.1);
-	$("#line6").fitText(1);
-	$("#line7").fitText(0.6);
-	$("#line8").fitText(1.2);
-	$("#line9").fitText(.9);
+	$("#line6").fitText(.9);
+	$("#line7").fitText(0.4);
+	$("#line8").fitText(1);
+	$("#line9").fitText(.725);
+	$("#business").fitText(.475);
+
+	$("#tell").fitText(.725);
+	$("#about").fitText(.7);
+	$("#assessment").fitText(.625);
+	$("#and").fitText(.65);
 });
